9982275df4f8dacdc3f3dc4946a5a8309369197a
abidingf16 <abidinabiuzyy@gmail.com>
2024-05-10 13:39:18 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let element1 = document.querySelector("p")
+function qirmizi() {
+    element1.style.color = "red"
+}
+
+function green() {
+    element1.style.color = "green"
+}
+
+function mavi() {
+    element1.style.color = "blue"
+}
---
+++ task2.js
@@ -0,0 +1,9 @@
+let element1 = document.getElementById("sa")
+function salam() {
+    element1.innerHTML = "Salam"
+}
+
+let element2 = document.getElementById("so")
+function sagol() {
+    element2.innerHTML = ""
+}
